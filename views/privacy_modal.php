e4df2985584b50f9171727b84e4a594808e5cc23
maksverbickij2005 <maksverbickij2005@gmail.com>
2025-03-20 10:37:42 +0300

     

--- views/privacy_modal.php
+++
@@ -1,9 +0,0 @@
-<?php
-// views/privacy_modal.php
-?>
-<div id="privacy-modal" class="modal">
-    <div class="modal-content">
-        <span class="close">&times;</span>
-        <?php include '../includes/privacy_policy.php'; ?>
-    </div>
-</div>
